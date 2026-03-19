a04c4bc3c5a0ebfbd06a982df2c91860925a2b56
Andrew Shershunov <the.evilur@gmail.com>
2026-03-20 00:25:10 +0300

    refactor: Get HavePinsInRange method with reflections once instead of multiple times

--- src/Core/Pin.cs
+++ src/Core/Pin.cs
@@ -1,8 +1,12 @@
 using UnityEngine;
 using HarmonyLib;
+using System.Reflection;
 
 namespace HotPins.Core {
     internal static class Pin {
+        private static MethodInfo _havePinInRange =
+            AccessTools.Method(typeof(Minimap), "HavePinInRange");
+
         public static void Add(Vector3 pos,
                                Minimap.PinType type,
                                string name) {
@@ -10,9 +14,9 @@ namespace HotPins.Core {
             if (Minimap.instance == null) return;
 
             /* If there is already such a pin */
-            if ((bool)AccessTools.Method(typeof(Minimap), "HavePinInRange")
-                                 .Invoke(Minimap.instance,
-                                         new object[] { pos, 2.5f })) return;
+            if ((bool)_havePinInRange.Invoke(Minimap.instance,
+                                             new object[] { pos, 2.5f }))
+                return;
 
             /* Add the pin to the map */
             Minimap.instance.AddPin(pos, type, name, true, false);
